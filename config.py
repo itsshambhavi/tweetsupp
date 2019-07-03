c59e99096ebc19e4077fc273321b92aa6863c972
RohanFouzdar <41817077+RohanFouzdar@users.noreply.github.com>
2019-07-03 15:50:05 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+consAPIkey = 'uAdLWYeMGRisw3W6mxlGNdRaX'
+consAPIsecretkey ='1y3wOqEkQKnr8MJciVqFxszUbtYdW5h0rofuUsvUo69Xh7ZYbh'
+accesstoken = '1146260188283822080-llu0zl8s8Kyevl8Qu9UCvoHjsT3SuG'
+accesstokensecret = 'xp38QOOC2Q4coxCW8LzidnsszIvP3kEWR591cxKluGzjI'
+
